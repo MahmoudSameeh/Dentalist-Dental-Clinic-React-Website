3929a3826973d27ae5391b0582c97929e82ef5fc
Mahmoud Sameeh <mahmoud.sameehwork@gmail.com>
2025-01-19 19:14:34 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Dentalist-Dental-Clinic-React-Website">
       <App />
     </BrowserRouter>
   </React.StrictMode>
